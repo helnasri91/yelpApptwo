4b14ddc173e534e1d714a9384259bfd221152bd7
GioEpic123 <giovanni.quevedo5@gmail.com>
2020-08-04 16:14:40 -0700

    Added a joke path, /gio

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/cat', (req,res) => {
     res.send('Roar!');
 });
 
+app.get('/gio' (req, res) => {
+		res.send("This is My path. It's Mine. Leave now.");
+})
+
 app.listen(3030, (err) =>
 	console.log(`${err ? err : 'Running on port 3030'}`),
-);git 
+);git
