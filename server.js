4a6070ee8b5f8cec241b932a3fb7eda396a59c3f
Hassan Aziz Elnasri <hassanazizelnasri@Galaxy-S8.hitronhub.home>
2020-08-04 11:50:38 -0700

    Yelp server upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.get('/cat', (req,res) => {
+    res.send('Roar!');
+});
+
+app.listen(3030, (err) =>
+	console.log(`${err ? err : 'Running on port 3030'}`),
+);
