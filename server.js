8f3f2f2aba973991a209c6e6bbd60391a6ceb29d
Helnasri91 <hassanazizelnasri@gmail.com>
2020-08-05 14:28:34 -0700

    business search from github API walkthrough

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+
+
 const express = require('express');
 
 const app = express();
+
+//Tells Node that we want to be able to accept json files into express
+app.use(express.json)
+//Same, except this allows form data. Extended is for nested objects within json files
+app.use(express.urlencoded({ extended: false}))
+
 
 app.get('/', (req, res) => {
 	res.send('Hello');
@@ -12,4 +20,20 @@ app.get('/cat', (req,res) => {
 
 app.listen(3030, (err) =>
 	console.log(`${err ? err : 'Running on port 3030'}`),
-);git 
+);
+
+
+'use strict';
+
+const yelp = require('yelp-fusion');
+const client = yelp.client('8HK6eeaWVT8HC1r9Ekuub33eP8OVbDrhWy_TGyjF7fdbQNzhSPiVyJyoEqgXioekmG7l5RoNfbqDmyNk3tu2CK2tEcyWBj0YfbpgpxNFNtQ4BDDb84Sv1MH5lhorX3Yx');
+
+client.search({
+  term: 'Four Barrel Coffee',
+  location: 'san francisco, ca',
+}).then(response => {
+  console.log(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
+
