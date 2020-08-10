f4985b63c8ac9006ebcd26d9d762f01c7894530f
GioEpic123 <giovanni.quevedo5@gmail.com>
2020-08-10 10:38:55 -0700

    Added a FoodList Route, and begin to get api calls sucessfully

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require('express');//Package express()
 const PORT = process.env.PORT || 3030
 
 require('dotenv').config()
 const app = express();
 
-//Add Router to Home (Index)
+const indexRouter = require('./routes/index') //Will Route user to index.js file
+const foodListRouter = require('./routes/foodList')
 //Add Router to Food List (By Zip)
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-	res.send('Hello');
-});
+// app.get('/', (req, res) => {
+// 	res.send('Hello');
+// });
 
-app.get('/cat', (req,res) => {
+app.get('/cat', (req, res) => {
     res.send('Roar!');
 });
 
@@ -25,11 +27,11 @@ app.get('/gio', (req, res) => {
 		res.send("This is My path. It's Mine. Leave now.");
 })
 
-/*
-app.use('/', indexRouter);
-app.use('/movies', moviesRouter);
-*/
+
+app.use('/', indexRouter); //When User hits '/' (main page) use indexRouter
+app.use('/food', foodListRouter);
+
 
 app.listen(PORT, (err) =>
 	console.log(`${err ? err : `Running on port ${PORT}`}`),
-);git
+)
